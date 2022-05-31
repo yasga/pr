a05072bebb07352698518f6c75b31607499c10e5
yasga <96418555+yasga@users.noreply.github.com>
2022-05-31 02:13:56 +0200

    Update DashboardScreen.js

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -88,10 +88,10 @@ export default function DashboardScreen() {
               <Card>
                 <Card.Body>
                   <Card.Title>
-                    $
+                    
                     {summary.orders && summary.users[0]
                       ? summary.orders[0].totalSales.toFixed(2)
-                      : 0}
+                      : 0} DH
                   </Card.Title>
                   <Card.Text>total des ventes</Card.Text>
                 </Card.Body>
