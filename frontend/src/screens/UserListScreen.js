2f7932d8a1f5196e622b1ed86be0f61b0c162e4a
yasga <96418555+yasga@users.noreply.github.com>
2022-05-31 03:23:21 +0200

    Update UserListScreen.js

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -116,7 +116,7 @@ export default function UserListScreen() {
                 <td>{user._id}</td>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>{user.isAdmin ? 'YES' : 'NO'}</td>
+                <td>{user.isAdmin ? 'OUI' : 'NON'}</td>
                 <td>
                   <Button
                     type="button"
