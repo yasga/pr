12a70da9e7b4b1dec5854a8454013fdb925a112f
yasga <96418555+yasga@users.noreply.github.com>
2022-06-09 13:37:54 +0200

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -38,7 +38,7 @@ function Product(props) {
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text className="prix">{product.price}MAD</Card.Text>
+        <Card.Text className="prix">{product.price} MAD</Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
             Out of stock
